2c3bbb7402e1f5962c43e40c28b8782f22d3c2ae
Sacha Duvivier <sduvivier@walibuy.com>
2024-01-28 14:30:32 +0100

    adding json

--- src/main/java/com/fges/todoapp/commands/Command.java
+++ src/main/java/com/fges/todoapp/commands/Command.java
@@ -17,26 +17,43 @@ abstract public class Command implements CommandInterface {
 
     public Path filePath;
 
-    public Command(String cmd, OptionsParser op, String fileContent, Path filePath) throws IOException {
-
+    public Command(String cmd, OptionsParser op, String fileContent, Path filePath) throws Exception {
         this.cmd = cmd;
         this.op = op;
         this.fileContent = fileContent;
         this.filePath = filePath;
+
+        if (!isCommand()) return;
+        if (op.getPositionalArgs().size() < this.neededArgs()) {
+            throw new Exception("Not enougth arguments for the command " + support());
+        }
+        exec();
+
     }
 
     public boolean isCommand() {
         return support().equals(cmd);
     }
 
-    public boolean exec() throws Exception {
-        if (!isCommand()) return false;
-        if (op.getPositionalArgs().size() < this.neededArgs()) {
-            throw new Exception("Not enougth arguments for the command " + support());
-        }
-        return true;
-    }
+    public void execCSV(String todo) throws Exception {}
 
+    public void execJSON(String todo) throws Exception {}
+
+    public void exec() throws Exception {
+        String todo = op.getPositionalArgs().size() > 1 ? op.getPositionalArgs().get(1) : null ;
+
+        String fileName = op.getOptions().get("fileName");
+        if (fileName.endsWith(".json")) {
+            execJSON(todo);
+
+            return;
+        } else if (fileName.endsWith(".csv")) {
+            execCSV(todo);
+
+            return;
+        }
+        throw new Exception("Sorry, only json & csv are accepted");
+    }
     public String support() {
         return null;
     }
