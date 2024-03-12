ed2f5a5a4635948a91b402863704cec2e67fe439
Sacha Duvivier <sduvivier@walibuy.com>
2024-03-12 15:26:43 +0100

    trying test

--- startCustom.sh
+++ startCustom.sh
@@ -1,10 +1,7 @@
 rm *.csv
 
-./exec.sh insert -s tmp--6639512648690044190.csv --done "If you’re going to call me names, I would prefer Ross, the Divorce Force. It’s just cooler."
-./exec.sh insert -s tmp--6639512648690044190.csv "You know what’s weird? Donald Duck never wore pants. But whenever he’s getting out of the shower, he always puts a towel around his waist. I mean, what is that about?"
-./exec.sh insert -s tmp--6639512648690044190.csv --done "\"It works on my machine\" always holds true for Chuck Norris."
-./exec.sh insert -s tmp-2296845861594962764.csv --done "How you doin’?"
-./exec.sh insert -s tmp-2296845861594962764.csv "\"It works on my machine\" always holds true for Chuck Norris."
-./exec.sh migrate -s tmp--6639512648690044190.csv -o tmp-2296845861594962764.csv
-./exec.sh list -s tmp-2296845861594962764.csv --done
-
+./exec.sh insert -s tmp-4548038250487246284.csv --done "Your collective dating record reads like a who’s who of human crap."
+./exec.sh insert -s tmp-4548038250487246284.csv "No! No, Joey! U-N-I-sex."
+./exec.sh insert -s tmp-6728962803849607679.csv --done "\"It works on my machine\" always holds true for Chuck Norris."
+./exec.sh migrate -s tmp-4548038250487246284.csv -o tmp-6728962803849607679.csv
+./exec.sh list -s tmp-6728962803849607679.csv
